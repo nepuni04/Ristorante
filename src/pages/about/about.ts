ef1a5b9739b1d768bcb7c64fcb1a35da25a06503
admin <admin>
2017-09-06 18:00:10 +0530

    Task one

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { Leader } from '../../shared/leader';
+import { LeaderProvider } from '../../providers/leader/leader';
 /**
  * Generated class for the AboutPage page.
  *
@@ -13,13 +15,23 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   selector: 'page-about',
   templateUrl: 'about.html',
 })
-export class AboutPage {
+export class AboutPage implements OnInit {
+  leaders: Leader[];
+  learderErrMess: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(public navCtrl: NavController, 
+    public navParams: NavParams, 
+    private leaderProvider: LeaderProvider,
+    @Inject("BaseURL") public BaseURL
+  ) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AboutPage');
+  }
+
+  ngOnInit() {
+    this.leaderProvider.getLeaders().subscribe(leaders => this.leaders = leaders, 
+      errmsg => this.learderErrMess = errmsg);
   }
 
 }
